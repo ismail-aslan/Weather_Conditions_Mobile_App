1a8ab828e90edea04f4cfa5e24814bcc67f2bf97
Ismail-Aslan <81377400+Ismail-Aslan@users.noreply.github.com>
2021-06-04 15:40:05 +0300

    AsyncTask created to get the current weather condition in JSON format from "openweathermap.org"

--- What_Is_The_Weather/app/src/main/java/com/example/whatistheweather/MainActivity.java
+++ What_Is_The_Weather/app/src/main/java/com/example/whatistheweather/MainActivity.java
@@ -2,13 +2,68 @@ package com.example.whatistheweather;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+
+    public class DownloadTask extends AsyncTask<String,Void,String>{
+
+        @Override
+        protected String doInBackground(String... urls) {
+            String result = "";
+            URL url;
+            HttpURLConnection urlConnection;
+
+            try {
+
+                url = new URL(urls[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream inputStream = urlConnection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(inputStream);
+                int data = reader.read();
+
+                while (data != -1){
+                    char current = (char) data;
+
+                    result += current;
+
+                    data = reader.read();
+
+                }
+
+                return result;
+
+            }catch (Exception e){
+                e.printStackTrace();
+                return null;
+            }
+
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            Log.i("result : : :",s);
+
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DownloadTask task = new DownloadTask();
+
+        task.execute("http://api.openweathermap.org/data/2.5/weather?q=KoNya&appid=54248234348f710f0f883f809d689635");
+
     }
 }
